bac72cb7cc21954d0ea84a4c38504b22323214c1
Erik <erik@r.com>
2020-09-27 17:06:21 +0300

    Switch to spring initializr startup

---
+++ src/main/java/fi/eriran/graphqltraining/GraphqlTrainingApplication.java
@@ -0,0 +1,13 @@
+package fi.eriran.graphqltraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlTrainingApplication.class, args);
+	}
+
+}
