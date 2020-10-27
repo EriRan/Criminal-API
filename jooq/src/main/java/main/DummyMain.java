50533440d4e792b8aa907b40b4f4cf33d351349f
Erik Rantanen <erik.rantanen@hotmail.fi>
2020-10-27 19:15:33 +0200

    Simplify submodule names
    New submodule names: core and jooq. Now make
    crashes when dependencies to jooq module
    are not found. Strange since they can be found in
    the IDE when in classes of the core module

---
+++ jooq/src/main/java/main/DummyMain.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Not in any use. Here just because Spring Boot demands that it exists
+ */
+public class DummyMain {
+    public static void main(String[] args) {
+
+    }
+}
