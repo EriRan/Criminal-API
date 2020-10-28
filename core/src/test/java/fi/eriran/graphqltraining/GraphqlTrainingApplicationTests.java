0d502adc57182bdd1d228a7a0b8c487bd6d6e5bc
Erik Rantanen <erik.rantanen@hotmail.fi>
2020-10-28 19:23:49 +0200

    Add test specific properties file
    Now we can re-enable spring default test

--- core/src/test/java/fi/eriran/graphqltraining/GraphqlTrainingApplicationTests.java
+++ core/src/test/java/fi/eriran/graphqltraining/GraphqlTrainingApplicationTests.java
@@ -2,12 +2,17 @@ package fi.eriran.graphqltraining;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(
+        locations = "classpath:hikari.properties"
+)
 class GraphqlTrainingApplicationTests {
 
-	//@Test
-	//void contextLoads() {
-	//}
+	@Test
+	void contextLoads() {
+		//Just check that the Spring is able to initialize
+	}
 
 }
