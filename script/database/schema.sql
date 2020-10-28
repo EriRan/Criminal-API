148902fa0fd8de823d087fbee24531574112c7ef
Erik Rantanen <erik.rantanen@hotmail.fi>
2020-10-28 14:17:59 +0200

    Change column name

--- script/database/schema.sql
+++ script/database/schema.sql
@@ -3,11 +3,11 @@ create schema criminal;
 CREATE TABLE criminal.criminal (
 	id serial NOT NULL,
 	"name" varchar(500) NOT NULL,
-	description text NULL,
+	appearance text NULL,
 	CONSTRAINT criminal_pk PRIMARY KEY (id)
 );
 COMMENT ON TABLE criminal.criminal IS 'Contains people who have committed crimes';
-COMMENT ON COLUMN criminal.criminal.description IS 'What does the criminal look like';
+COMMENT ON COLUMN criminal.criminal.appearance IS 'What does the criminal look like';
 
 CREATE TABLE criminal.charge (
 	id serial NOT NULL,
@@ -21,13 +21,13 @@ COMMENT ON TABLE criminal.charge IS 'A crime a criminal has been charged with';
 -- Initial data
 
 INSERT INTO criminal.criminal
-("name", description)
+("name", appearance)
 VALUES('Osama bin Laden', 'Long black beard');
 INSERT INTO criminal.criminal
-("name", description)
+("name", appearance)
 VALUES('Slick Joe', 'Handsome face, white mohawk, red shirt with a text "Ouch!" on it');
 INSERT INTO criminal.criminal
-("name", description)
+("name", appearance)
 VALUES('Robert von Lomboc', 'Slim stature. Always wearing white English judge wig');
 
 INSERT INTO criminal.charge
