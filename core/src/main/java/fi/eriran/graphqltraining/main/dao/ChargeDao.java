7c304d6c553e3bd65c9a1597ba6e1e5c4fc22ae7
Erik Rantanen <erik.rantanen@hotmail.fi>
2020-10-28 14:19:13 +0200

    Do database queries with Jooq
    These are really simple queries so they could be
    done automatically with Hibernate. Look into that
    in future

--- core/src/main/java/fi/eriran/graphqltraining/main/dao/ChargeDao.java
+++ core/src/main/java/fi/eriran/graphqltraining/main/dao/ChargeDao.java
@@ -1,40 +1,21 @@
 package fi.eriran.graphqltraining.main.dao;
 
+import fi.eriran.generated.jooq.Tables;
 import fi.eriran.graphqltraining.main.pojo.Charge;
+import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.sql.DataSource;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Collection;
 
 @Component
 public class ChargeDao {
 
     @Autowired
-    private DataSource dataSource;
+    private DSLContext ctx;
 
-    public Collection<Charge> getCharges(Integer criminalId) throws SQLException {
-        try (PreparedStatement statement = dataSource.getConnection()
-                .prepareStatement("select id, description from criminal.charge where criminal_id = ?")) {
-            statement.setInt(1, criminalId);
-            statement.execute();
-            return mapResults(statement.getResultSet());
-        }
+    public Collection<Charge> getCharges(Integer criminalId) {
+        return ctx.selectFrom(Tables.CHARGE).where(Tables.CHARGE.CRIMINAL_ID.eq(criminalId))
+                .fetchInto(Charge.class);
     }
-
-    private Collection<Charge> mapResults(ResultSet resultSet) throws SQLException {
-        Collection<Charge> charges = new ArrayList<>();
-        while (resultSet.next()) {
-            Charge newCharge = new Charge();
-            newCharge.setId(resultSet.getInt(1));
-            newCharge.setDescription(resultSet.getString(2));
-            charges.add(newCharge);
-        }
-        return charges;
-    }
-
 }
--- core/src/main/java/fi/eriran/graphqltraining/main/dao/CriminalDao.java
+++ core/src/main/java/fi/eriran/graphqltraining/main/dao/CriminalDao.java
@@ -1,34 +1,20 @@
 package fi.eriran.graphqltraining.main.dao;
 
+import fi.eriran.generated.jooq.Tables;
 import fi.eriran.graphqltraining.main.pojo.Criminal;
+import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import javax.sql.DataSource;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 @Component
 public class CriminalDao {
 
     @Autowired
-    private DataSource dataSource;
+    private DSLContext ctx;
 
-    public Criminal getCriminal(Integer id) throws SQLException {
-        try (PreparedStatement statement = dataSource.getConnection()
-                .prepareStatement("select name, description from criminal.criminal where id = ?")) {
-            statement.setInt(1, id);
-            statement.execute();
-            ResultSet results = statement.getResultSet();
-            if (!results.next()) {
-                return new Criminal();
-            }
-            Criminal criminal = new Criminal();
-            criminal.setId(id);
-            criminal.setName(results.getString(1));
-            criminal.setAppearance(results.getString(2));
-            return criminal;
-        }
+    public Criminal getCriminal(Integer id) {
+        return ctx.selectFrom(Tables.CRIMINAL_)
+                .where(Tables.CRIMINAL_.ID.eq(id))
+                .fetchOneInto(Criminal.class);
     }
 }
