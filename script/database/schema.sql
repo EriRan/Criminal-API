838a6620f209a0ca678e8a3cd916b9228cf79c3a
Erik Rantanen <erik.rantanen@hotmail.fi>
2020-11-01 19:05:20 +0200

    Expand database schema
    Added sightings table, crime_type table and expanded charge table

--- script/database/schema.sql
+++ script/database/schema.sql
@@ -9,16 +9,52 @@ CREATE TABLE criminal.criminal (
 COMMENT ON TABLE criminal.criminal IS 'Contains people who have committed crimes';
 COMMENT ON COLUMN criminal.criminal.appearance IS 'What does the criminal look like';
 
+CREATE TABLE criminal.crime_type (
+	id serial NOT NULL,
+	"name" varchar NOT NULL,
+	CONSTRAINT crime_type_pk PRIMARY KEY (id)
+);
+COMMENT ON TABLE criminal.criminal IS 'Possible crimes that a criminal can commit';
+
 CREATE TABLE criminal.charge (
 	id serial NOT NULL,
 	description text NOT NULL,
 	criminal_id int4 NOT NULL,
+	created_at timestamptz(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	charged_at date NOT NULL,
+	punished_at date NULL,
+	crime_type_id int4 NOT NULL,
 	CONSTRAINT charge_pk PRIMARY KEY (id),
+	CONSTRAINT charge_crime_type_fk FOREIGN KEY (id) REFERENCES criminal.crime_type(id) ON UPDATE RESTRICT ON DELETE RESTRICT,
 	CONSTRAINT charge_fk FOREIGN KEY (criminal_id) REFERENCES criminal.criminal(id) ON UPDATE RESTRICT ON DELETE RESTRICT
 );
 COMMENT ON TABLE criminal.charge IS 'A crime a criminal has been charged with';
 
+CREATE TABLE criminal.sighting (
+	id serial NOT NULL,
+	criminal_id int4 NOT NULL,
+	area varchar(500) NULL,
+	address varchar(500) NULL,
+	description text NULL,
+	CONSTRAINT sighting_pk PRIMARY KEY (id),
+	CONSTRAINT sighting_criminal_fk FOREIGN KEY (id) REFERENCES criminal.criminal(id) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+COMMENT ON TABLE criminal.sighting IS 'Locations where a criminal has been spotted';
+
 -- Initial data
+
+INSERT INTO criminal.crime_type
+("name")
+VALUES('Financial');
+INSERT INTO criminal.crime_type
+("name")
+VALUES('Murder');
+INSERT INTO criminal.crime_type
+("name")
+VALUES('Unclassified');
+INSERT INTO criminal.crime_type
+("name")
+VALUES('Thievery');
 
 INSERT INTO criminal.criminal
 ("name", appearance)
@@ -31,11 +67,11 @@ INSERT INTO criminal.criminal
 VALUES('Robert von Lomboc', 'Slim stature. Always wearing white English judge wig');
 
 INSERT INTO criminal.charge
-(description, criminal_id)
-VALUES('Terrorism', 1);
+(description, criminal_id, crime_type_id, charged_at)
+VALUES('Terrorism', 1, 2, '2020-11-01');
 INSERT INTO criminal.charge
-(description, criminal_id)
-VALUES('Being way too cool', 2);
+(description, criminal_id, crime_type_id, charged_at)
+VALUES('Being way too cool', 2, 3, '2020-11-01');
 INSERT INTO criminal.charge
-(description, criminal_id)
-VALUES('Financial crimes', 3);
+(description, criminal_id, crime_type_id, charged_at)
+VALUES('Financial crimes', 3, 1, '2020-11-01');
