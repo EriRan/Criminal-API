cc3cd42b777c2f288a7949e5c22ac9d7a99cd249
Erik Rantanen <erik.rantanen@hotmail.fi>
2020-11-15 18:14:15 +0200

    Deserialize response

--- core/src/test/java/fi/eriran/criminalapi/CriminalTest.java
+++ core/src/test/java/fi/eriran/criminalapi/CriminalTest.java
@@ -1,15 +1,21 @@
 package fi.eriran.criminalapi;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.k0kubun.builder.query.graphql.GraphQL;
 import com.graphql.spring.boot.test.GraphQLResponse;
 import com.graphql.spring.boot.test.GraphQLTestTemplate;
+import fi.eriran.criminalapi.main.pojo.Criminal;
 import fi.eriran.criminalapi.testutil.annotation.CriminalApiSpringBootTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @CriminalApiSpringBootTest
@@ -19,7 +25,7 @@ class CriminalTest {
     private GraphQLTestTemplate testTemplate;
 
     @Test
-    void requestedVariablesFetched() {
+    void requestedVariablesFetched() throws JsonProcessingException {
         Map<String, Object> criminalParams = new HashMap<>();
         criminalParams.put("id", "1");
 
@@ -35,5 +41,16 @@ class CriminalTest {
                                 .build() + "}",
                         "{}");
         assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(response.getRawResponse().getBody());
+        JsonNode responseData = jsonNode.get("data").get("criminalById");
+
+        Criminal criminal = mapper.readValue(responseData.toString(), Criminal.class);
+
+        assertNotNull(criminal);
+        assertNotNull(criminal.getId());
+        assertNotNull(criminal.getName());
+        assertNotNull(criminal.getAppearance());
     }
 }
