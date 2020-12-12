befd6b4fd7309ed87d9907298d0bdf932fafee68
Erik Rantanen <erik.rantanen@hotmail.fi>
2020-12-12 17:21:55 +0200

    Use TestRestTemplate instead of mocked user

--- core/src/test/java/fi/eriran/criminalapi/main/rest/LoginControllerTest.java
+++ core/src/test/java/fi/eriran/criminalapi/main/rest/LoginControllerTest.java
@@ -1,41 +1,27 @@
 package fi.eriran.criminalapi.main.rest;
 
 import fi.eriran.criminalapi.testcore.annotation.CriminalApiSpringBootTest;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.net.URI;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @CriminalApiSpringBootTest
 class LoginControllerTest {
 
     @Autowired
-    private WebApplicationContext context;
+    private TestRestTemplate testRestTemplate;
 
-    private MockMvc mvc;
-
-    @BeforeEach
-    public void setup() {
-        mvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .apply(springSecurity())
-                .build();
-    }
-
-    // ... other methods
-
-    @WithMockUser("spring")
     @Test
-    void loginShouldReturnOk() throws Exception {
-        mvc.perform(post("/login").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+    void loginShouldReturnOk() {
+        ResponseEntity<String> response = testRestTemplate
+                .withBasicAuth("spring", "spring")
+                .postForEntity(URI.create("/login"), null, String.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 }
