bb4e4073e77e5ac0086e9b5e610dcba582946631
Erik Rantanen <erik.rantanen@hotmail.fi>
2020-12-12 15:39:03 +0200

    Change HTTPSecurity configuration
    Permit all requests to base url and login but authenticate the rest.
    GraphQL requests returning 401 which is a promising sight from 403

--- core/src/main/java/fi/eriran/criminalapi/configuration/WebSecurityConfig.java
+++ core/src/main/java/fi/eriran/criminalapi/configuration/WebSecurityConfig.java
@@ -4,18 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/")
-                .hasRole("USER")
+                .antMatchers("/", "/login")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
                 .and()
                 .httpBasic();
     }
