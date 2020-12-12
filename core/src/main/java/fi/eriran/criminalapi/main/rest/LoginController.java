cbcd255d2653ce2b032757cfc91ffffbc2686a4d
Erik Rantanen <erik.rantanen@hotmail.fi>
2020-12-12 17:09:43 +0200

    Add LoginController
    Mainly to figure out whether the spring boot security issues Im having
    are related only to graphql or REST too

---
+++ core/src/main/java/fi/eriran/criminalapi/main/rest/LoginController.java
@@ -0,0 +1,15 @@
+package fi.eriran.criminalapi.main.rest;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    @PostMapping
+    public String login() {
+        return "Hello!";
+    }
+}
---
+++ core/src/test/java/fi/eriran/criminalapi/main/rest/LoginControllerTest.java
@@ -0,0 +1,41 @@
+package fi.eriran.criminalapi.main.rest;
+
+import fi.eriran.criminalapi.testcore.annotation.CriminalApiSpringBootTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@CriminalApiSpringBootTest
+class LoginControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    // ... other methods
+
+    @WithMockUser("spring")
+    @Test
+    void loginShouldReturnOk() throws Exception {
+        mvc.perform(post("/login").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
