ebe60e415f1dfe6b9f9448182b32fcf7742f4334
Erik Rantanen <erik.rantanen@hotmail.fi>
2020-12-13 14:00:36 +0200

    Remove index part
    Just a single '/' is needed for the index.html to be always viewable

--- core/src/main/java/fi/eriran/criminalapi/configuration/security/SecurityConfiguration.java
+++ core/src/main/java/fi/eriran/criminalapi/configuration/security/SecurityConfiguration.java
@@ -13,7 +13,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "index", "/greeting")
+                .antMatchers("/", "/greeting")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
