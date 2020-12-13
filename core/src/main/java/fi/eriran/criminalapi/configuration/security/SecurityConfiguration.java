19a061eeb7f4c20782415216c5847bf272cad0f9
Erik Rantanen <erik.rantanen@hotmail.fi>
2020-12-13 15:05:35 +0200

    Add BCryptPasswordEncoder
    After this the passwords no longer seemed to work.

--- core/src/main/java/fi/eriran/criminalapi/configuration/security/SecurityConfiguration.java
+++ core/src/main/java/fi/eriran/criminalapi/configuration/security/SecurityConfiguration.java
@@ -1,13 +1,26 @@
 package fi.eriran.criminalapi.configuration.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfiguration(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,5 +32,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.builder()
+                .username("cool")
+                .password(passwordEncoder.encode("guy"))
+                .roles("USER").build();
+
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
