5ad19a98b876abeb944b6514cddd5998ec597a74
Erik Rantanen <erik.rantanen@hotmail.fi>
2020-12-14 19:26:03 +0200

    Add table annotation
    Hoping this would resolve the issue with insertQuery but no luck

--- core/src/main/java/fi/eriran/criminalapi/main/pojo/user/NewUser.java
+++ core/src/main/java/fi/eriran/criminalapi/main/pojo/user/NewUser.java
@@ -1,10 +1,12 @@
 package fi.eriran.criminalapi.main.pojo.user;
 
 import javax.persistence.Column;
+import javax.persistence.Table;
 
 /**
  * User object that contains the variables needed to create a new user
  */
+@Table(name = "user")
 public class NewUser {
 
     @Column(name = "username")
