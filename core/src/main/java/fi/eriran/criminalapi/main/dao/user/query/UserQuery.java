1bba981e0133a0a612bc5f1a662fc553259aa46d
Erik Rantanen <erik.rantanen@hotmail.fi>
2020-12-14 20:02:53 +0200

    Revert "Use regular insert statement instead"
    This reverts commit 10b6e630

--- core/src/main/java/fi/eriran/criminalapi/main/dao/user/query/UserQuery.java
+++ core/src/main/java/fi/eriran/criminalapi/main/dao/user/query/UserQuery.java
@@ -3,7 +3,10 @@ package fi.eriran.criminalapi.main.dao.user.query;
 import fi.eriran.criminalapi.main.dao.common.query.AbstractQuery;
 import fi.eriran.criminalapi.main.pojo.user.NewUser;
 import fi.eriran.generated.jooq.tables.records.UserRecord;
-import org.jooq.*;
+import org.jooq.DSLContext;
+import org.jooq.InsertQuery;
+import org.jooq.SelectConditionStep;
+import org.jooq.UpdateConditionStep;
 import org.springframework.stereotype.Component;
 
 import static fi.eriran.generated.jooq.tables.User.USER;
@@ -15,11 +18,11 @@ public class UserQuery extends AbstractQuery {
         super(ctx);
     }
 
-    public InsertResultStep<UserRecord> insert(NewUser newUser) {
-        return ctx.insertInto(USER)
-                .set(USER.USERNAME, newUser.getUserName())
-                .set(USER.PASSWORD, newUser.getPassword())
-                .returning();
+    public InsertQuery<UserRecord> insert(NewUser newUser) {
+        InsertQuery<UserRecord> insertQuery = ctx.insertQuery(USER);
+        insertQuery.addRecord(createInsertRecord(newUser));
+        insertQuery.setReturning();
+        return insertQuery;
     }
 
     public SelectConditionStep<UserRecord> select(Integer userId) {
@@ -28,5 +31,12 @@ public class UserQuery extends AbstractQuery {
 
     public UpdateConditionStep<UserRecord> updatePassword(Integer userId, String newPassword) {
         return ctx.update(USER).set(USER.PASSWORD, newPassword).where(USER.ID.eq(userId));
+    }
+
+    private UserRecord createInsertRecord(NewUser newUser) {
+        UserRecord userRecord = new UserRecord();
+        userRecord.setUsername(newUser.getUserName());
+        userRecord.setPassword(newUser.getPassword());
+        return userRecord;
     }
 }
