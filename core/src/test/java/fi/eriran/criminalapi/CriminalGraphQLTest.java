4e77021d2e7db205383e5d1fc7ce3c3453c26344
Erik Rantanen <erik.rantanen@hotmail.fi>
2021-01-06 15:18:13 +0200

    Merge tests
    CriminalGraphQLTest was testing a single simple case. CriminalTest
    tested the same but with more values

--- core/src/test/java/fi/eriran/criminalapi/CriminalGraphQLTest.java
+++ core/src/test/java/fi/eriran/criminalapi/CriminalGraphQLTest.java
@@ -1,28 +1,39 @@
 package fi.eriran.criminalapi;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.graphql.spring.boot.test.GraphQLResponse;
-import com.graphql.spring.boot.test.GraphQLTest;
 import com.graphql.spring.boot.test.GraphQLTestTemplate;
-import fi.eriran.criminalapi.configuration.ScalarConfiguration;
 import fi.eriran.criminalapi.main.dao.ChargeDao;
 import fi.eriran.criminalapi.main.dao.CriminalDao;
 import fi.eriran.criminalapi.main.dao.SightingDao;
 import fi.eriran.criminalapi.main.pojo.Criminal;
 import fi.eriran.criminalapi.testcore.annotation.CriminalApiGraphQLTest;
-import fi.eriran.criminalapi.testcore.util.filepath.QueryFilePathProvider;
-import org.junit.jupiter.api.Disabled;
+import fi.eriran.criminalapi.testcore.deserialization.GraphQLResponseDeserializer;
+import fi.eriran.criminalapi.testcore.testdata.ChargeTestObjectFactory;
+import fi.eriran.criminalapi.testcore.testdata.CriminalTestObjectFactory;
+import fi.eriran.criminalapi.testcore.testdata.SightingTestObjectFactory;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
+import static com.github.k0kubun.builder.query.graphql.GraphQLQueryBuilder.object;
+import static com.github.k0kubun.builder.query.graphql.GraphQLQueryBuilder.query;
+import static fi.eriran.criminalapi.testcore.query.GraphQLQueryBuilderJsonWrapper.wrap;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
+/**
+ * Ensure that queries and the response deserialization works correctly for Criminal related objects
+ */
 @CriminalApiGraphQLTest
 class CriminalGraphQLTest {
 
@@ -36,15 +47,155 @@ class CriminalGraphQLTest {
     @Autowired
     private GraphQLTestTemplate testTemplate;
 
-    @Test
-    void getOnlyCriminal() throws IOException {
-        when(criminalDao.get(any())).thenReturn(new Criminal());
-        when(sightingDao.getSightings(any())).thenReturn(new ArrayList<>());
-        when(chargeDao.getCharges(any())).thenReturn(new ArrayList<>());
+    @BeforeEach
+    public void initMocks() {
+        when(criminalDao.get(anyInt()))
+                .thenReturn(CriminalTestObjectFactory.create(1));
+        when(chargeDao.getCharges(anyInt()))
+                .thenReturn(
+                        Collections.singletonList(
+                                ChargeTestObjectFactory.create(1)
+                        )
 
-        GraphQLResponse response = testTemplate.postForResource(
-                new QueryFilePathProvider().provide("criminal")
-        );
+                );
+        when(sightingDao.getSightings(anyInt()))
+                .thenReturn(
+                        Collections.singletonList(
+                                SightingTestObjectFactory.create(1)
+                        )
+
+                );
+    }
+
+    @Test
+    void criminalVariablesFetched() throws JsonProcessingException {
+        Map<String, Object> criminalParams = new HashMap<>();
+        criminalParams.put("id", "1");
+        GraphQLResponse response = testTemplate
+                .postMultipart(
+                        wrap(
+                                query()
+                                        .object("criminalById", criminalParams, object()
+                                                .field("id")
+                                                .field("name")
+                                                .field("appearance")
+                                                .build())
+                                        .build()
+                        ),
+                        "{}");
         assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        Criminal criminal = GraphQLResponseDeserializer.deserialize(response, Criminal.class);
+
+        assertNotNull(criminal);
+        assertNotNull(criminal.getId());
+        assertNotNull(criminal.getName());
+        assertNotNull(criminal.getAppearance());
+
+        assertTrue(CollectionUtils.isEmpty(criminal.getCharges()));
+        assertTrue(CollectionUtils.isEmpty(criminal.getSightings()));
+    }
+
+    @Test
+    void notRequestedVariablesAreNotFetched() throws JsonProcessingException {
+        Map<String, Object> criminalParams = new HashMap<>();
+        criminalParams.put("id", "1");
+
+        //Just give me the name of the man!!!
+        GraphQLResponse response = testTemplate
+                .postMultipart(
+                        wrap(
+                                query()
+                                        .object("criminalById", criminalParams, object()
+                                                .field("name")
+                                                .build())
+                                        .build()
+                        ),
+                        "{}");
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        Criminal criminal = GraphQLResponseDeserializer.deserialize(response, Criminal.class);
+
+        assertNotNull(criminal);
+        assertNull(criminal.getId());
+        assertNotNull(criminal.getName());
+        assertNull(criminal.getAppearance());
+
+        assertTrue(CollectionUtils.isEmpty(criminal.getCharges()));
+        assertTrue(CollectionUtils.isEmpty(criminal.getSightings()));
+    }
+
+    @Test
+    void chargesFetched() throws JsonProcessingException {
+        Map<String, Object> criminalParams = new HashMap<>();
+        criminalParams.put("id", "1");
+
+        GraphQLResponse response = testTemplate
+                .postMultipart(
+                        wrap(
+                                query()
+                                        .object("criminalById", criminalParams, object()
+                                                .field("id")
+                                                .field("name")
+                                                .field("appearance")
+                                                .object("charges", object()
+                                                        .field("id")
+                                                        .field("description")
+                                                        .build())
+                                                .build())
+                                        .build()
+                        ),
+                        "{}");
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        Criminal criminal = GraphQLResponseDeserializer.deserialize(response, Criminal.class);
+
+        assertNotNull(criminal);
+        assertTrue(CollectionUtils.isEmpty(criminal.getSightings()));
+        assertFalse(CollectionUtils.isEmpty(criminal.getCharges()));
+        criminal.getCharges().forEach(charge -> {
+            assertNotNull(charge.getId());
+            assertFalse(StringUtils.isBlank(charge.getDescription()));
+        });
+    }
+
+    @Test
+    void sightingsFetched() throws JsonProcessingException {
+        Map<String, Object> criminalParams = new HashMap<>();
+        criminalParams.put("id", "1");
+
+        GraphQLResponse response = testTemplate
+                .postMultipart(
+                        wrap(
+                                query()
+                                        .object("criminalById", criminalParams, object()
+                                                .field("id")
+                                                .field("name")
+                                                .field("appearance")
+                                                .object("sightings", object()
+                                                        .field("id")
+                                                        .field("description")
+                                                        .field("timeOfSighting")
+                                                        .field("address")
+                                                        .field("area")
+                                                        .build())
+                                                .build())
+                                        .build()
+                        ),
+                        "{}");
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        Criminal criminal = GraphQLResponseDeserializer.deserialize(response, Criminal.class);
+
+        assertNotNull(criminal);
+        assertTrue(CollectionUtils.isEmpty(criminal.getCharges()));
+        assertFalse(CollectionUtils.isEmpty(criminal.getSightings()));
+        criminal.getSightings().forEach(sighting -> {
+            assertNotNull(sighting.getId());
+            assertFalse(StringUtils.isBlank(sighting.getDescription()));
+            assertFalse(StringUtils.isBlank(sighting.getAddress()));
+            assertFalse(StringUtils.isBlank(sighting.getArea()));
+            assertNotNull(sighting.getTimeOfSighting());
+        });
     }
 }
